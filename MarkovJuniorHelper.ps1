13a8c0aee8325ff123f29c88b7f25d7a8bfc8514
Camille Gouneau <27480916+Camille-Gouneau@users.noreply.github.com>
2024-06-06 18:16:01 +0200

    Improved Helper script with more options

--- MarkovJuniorHelper.ps1
+++ MarkovJuniorHelper.ps1
@@ -19,14 +19,19 @@ Disable the Gif Generation.
 .Parameter -RemovePNG
 Remove the png used to generate a gif. This is recommended as the amount of png can quickly take a lot of space.
 
+.Parameter -RemovePaletteAfterUse
+Remove the generated palette file after use.
+
 .Parameter -Silent
-Do not display subcommands output
+Do not display subcommands output.
 
 .Example
-MarkovJuniorHelper.ps1
+MarkovJuniorHelper.ps1 -OutputFolderName $(Get-Date -Format "yyyy_MM_dd_HH_mm_ss") -RemovePNG -RemovePaletteAfterUse -Silent
+This will create a subfolder with the current datetime.
 
 .Example
-MarkovJuniorHelper.ps1 -OutputFolderName myproject -RemovePNG -Silent
+MarkovJuniorHelper.ps1 -Clean
+This will delete all content inside the $OriginalOutputFolder/, output/ by default
 
 #>
 
@@ -36,7 +41,9 @@ Param(
     [switch] $NoPaletteGen,
     [switch] $NoGif,
     [switch] $RemovePNG,
-    [switch] $Silent
+    [switch] $RemovePaletteAfterUse,
+    [switch] $Silent,
+    [switch] $Clean
 )
 
 function prt {
@@ -57,11 +64,20 @@ function prt {
 
 $FullPath = "$OriginalOutputFolder/$OutputFolderName"
 
-prt $Silent Blue Black
+if ($Clean) {
+    Remove-Item -Path "$OriginalOutputFolder\*" -Recurse
+    Exit
+}
 
 if (!(Test-Path "$FullPath")) {
     prt "Folder not found at '${FullPath}'. Creating folder..." Yellow Black
-    New-Item -ItemType Directory -Path "$FullPath"
+    if ($Silent) {
+        New-Item -ItemType Directory -Path "$FullPath" | Out-Null
+    }
+    else {
+        New-Item -ItemType Directory -Path "$FullPath"
+    }
+    
 }
 
 prt "Running MarkovJunior..." Yellow Black
@@ -75,19 +91,19 @@ else {
 
 if (!$NoPaletteGen) {
     prt "Generating Palette..." Yellow Black
-    prt "ffmpeg -i $OriginalOutputFolder/0.png -vf palettegen $FullPath/palette.png" Red White
+    #prt "ffmpeg -i $OriginalOutputFolder/0.png -vf palettegen $FullPath/palette.png" Red White
     if ($Silent) {
         ffmpeg -y -i $OriginalOutputFolder/0.png -vf palettegen $FullPath/palette.png -hide_banner -loglevel error
     }
     else {
         ffmpeg -y -i $OriginalOutputFolder/0.png -vf palettegen $FullPath/palette.png
     }
-    prt "Palette generated at '${FullPath}/palette.png" Green Black
+    prt "Palette generated at '${FullPath}/palette.png !" Green Black
 }
 
 if (!$NoGif) {
     prt "Generating GIF..." Yellow Black
-    prt "ffmpeg -framerate 50 -i $OriginalOutputFolder/%d.png -i $FullPath/palette.png -lavfi paletteuse -sws_dither none -loop -1 $FullPath/output.gif" Red White
+    #prt "ffmpeg -framerate 50 -i $OriginalOutputFolder/%d.png -i $FullPath/palette.png -lavfi paletteuse -sws_dither none -loop -1 $FullPath/output.gif" Red White
     if ($Silent) {
         ffmpeg -framerate 50 -y -i $OriginalOutputFolder/%d.png -i $FullPath/palette.png -lavfi paletteuse -sws_dither none -loop -1 $FullPath/output.gif -hide_banner -loglevel error
     }
@@ -98,11 +114,21 @@ if (!$NoGif) {
 }
 
 if ($RemovePNG) {
-    prt "Cleaning files in folder $OriginalOutputFolder/" Yellow Black
+    prt "Cleaning files in folder $OriginalOutputFolder/..." Yellow Black
     if ($Silent) {
         Remove-Item .\$OriginalOutputFolder\*.png | Out-Null
     }
     else {
         Remove-Item .\$OriginalOutputFolder\*.png
+    }
+}
+
+if ($RemovePaletteAfterUse) {
+    prt "Removing $FullPath/palette.png..." Yellow Black
+    if ($Silent) {
+        Remove-Item .\$FullPath\palette.png | Out-Null
+    }
+    else {
+        Remove-Item .\$FullPath\palette.png
     }
 }
